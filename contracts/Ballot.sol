1544da78a98551b992d4e3d6ff1f5acc22369884
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2023-01-01 22:28:38 +0100

    Implements tests for the giveRightToVoteMethod

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -32,4 +32,12 @@ contract Ballot {
             }));
         }
     }
+
+    function giveRightToVote(address voter) external {
+        require(msg.sender == chairperson, "Only the chairperson can give right to vote");
+        require(!voters[voter].voted, "The voter already voted");
+        require(voters[voter].weight == 0);
+
+        voters[voter].weight = 1;
+    }
 }
--- test/Ballot.js
+++ test/Ballot.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Ballot", () => {
@@ -22,8 +22,6 @@ describe("Ballot", () => {
         });
 
         it("should set the chairperson's vote weight as 1", async () => {
-            const signer = await ethers.getSigner();
-    
             const factory = await ethers.getContractFactory("Ballot");
             const contract = await factory.deploy([BLUE_PROPOSAL, RED_PROPOSAL, GREEN_PROPOSAL]);
     
@@ -34,8 +32,6 @@ describe("Ballot", () => {
         });
 
         it("should set the vote proposals", async () => {
-            const signer = await ethers.getSigner();
-    
             const factory = await ethers.getContractFactory("Ballot");
             const contract = await factory.deploy([BLUE_PROPOSAL, RED_PROPOSAL, GREEN_PROPOSAL]);
     
@@ -48,6 +44,77 @@ describe("Ballot", () => {
             expect(thirdProposal.name).to.be.eq(GREEN_PROPOSAL);
         });
 
-    })
+        it("should not set a proposal that was not informed during contract deployment", async () => {
+            const factory = await ethers.getContractFactory("Ballot");
+            const contract = await factory.deploy([BLUE_PROPOSAL, RED_PROPOSAL, GREEN_PROPOSAL]);
+    
+            try {
+                await contract.proposals(3);
+                assert.fail("The try code should fail");
+            } catch (e) {
+                expect(e).to.be.a("Error");
+            }
+        });
+
+    });
+
+    describe("getRightToVote", () => {
+
+        it("should update the voter weight from 0 to 1", async () => {
+            const [_, accountTwo] = await ethers.getSigners();
+            
+            const factory = await ethers.getContractFactory("Ballot");
+            const contract = await factory.deploy([BLUE_PROPOSAL, RED_PROPOSAL, GREEN_PROPOSAL]);
+
+            const voterBeforeRight = await contract.voters(accountTwo.address);
+            expect(voterBeforeRight.weight).to.be.eq(0);
+
+            await contract.giveRightToVote(accountTwo.address);
+
+            const voterAfterRight = await contract.voters(accountTwo.address);
+            expect(voterAfterRight.weight).to.be.eq(1);
+        });
+
+        it("should revert when accessed by someone other than chairperson", async () => {
+            const [_, accountTwo] = await ethers.getSigners();
+            
+            const factory = await ethers.getContractFactory("Ballot");
+            const contract = await factory.deploy([BLUE_PROPOSAL, RED_PROPOSAL, GREEN_PROPOSAL]);
+
+            const contractConnectedToNonChairperson = contract.connect(accountTwo);
+
+            try {
+                await contractConnectedToNonChairperson.giveRightToVote(accountTwo.address);
+                assert.fail("The try code should fail");
+            } catch (e) {
+                expect(e).to.be.a("Error");
+                expect(e.message).to.be.eq("VM Exception while processing transaction: reverted with reason string 'Only the chairperson can give right to vote'");
+            }
+        });
+
+        it("should revert when voter has already voted", async () => {
+            // TODO I can only implement this test once I implement the vote method
+        });
+
+        it("should revert when voter's weight is greater than 0", async () => {
+            const [_, accountTwo] = await ethers.getSigners();
+            
+            const factory = await ethers.getContractFactory("Ballot");
+            const contract = await factory.deploy([BLUE_PROPOSAL, RED_PROPOSAL, GREEN_PROPOSAL]);
+
+            await contract.giveRightToVote(accountTwo.address);
+
+            // It should fail on the second time
+            // because the weight is already 1
+            try {
+                await contract.giveRightToVote(accountTwo.address);
+                assert.fail("The try code should fail");
+            } catch (e) {
+                expect(e).to.be.a("Error");
+                expect(e.message).to.be.eq("Transaction reverted without a reason string");
+            }
+        });
+
+    });
 
 });
